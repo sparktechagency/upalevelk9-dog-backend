7da39d77d47de3177c0f27520af5c11e5c165c30
Yeasin Arafat <leoarafat10@gmail.com>
2024-05-18 16:26:46 +0600

    add bearer in auth middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -7,29 +7,39 @@ import { jwtHelpers } from '../../helpers/jwtHelpers';
 const auth =
   (...requiredRoles: string[]) =>
   async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      //get authorization token
-      const token = req.headers.authorization;
-      if (!token) {
-        throw new ApiError(401, 'You are not authorized for this role');
-      }
-      // verify token
-      let verifiedUser = null;
+    const { authorization } = req.headers;
+    let token;
+    if (authorization && authorization.startsWith('Bearer')) {
+      try {
+        //get authorization token
+        token = authorization.split(' ')[1];
+        if (!token) {
+          throw new ApiError(401, 'You are not authorized for this role');
+        }
+        // verify token
+        let verifiedUser = null;
 
-      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
-
-      req.user = verifiedUser; // role , userid
-
-      // Guard for role
-      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
-        throw new ApiError(
-          403,
-          'Access Forbidden: You do not have permission to perform this action.',
+        verifiedUser = jwtHelpers.verifyToken(
+          token,
+          config.jwt.secret as Secret,
         );
+
+        req.user = verifiedUser; // role , userid
+
+        // Guard for role
+        if (
+          requiredRoles.length &&
+          !requiredRoles.includes(verifiedUser.role)
+        ) {
+          throw new ApiError(
+            403,
+            'Access Forbidden: You do not have permission to perform this action.',
+          );
+        }
+        next();
+      } catch (error) {
+        next(error);
       }
-      next();
-    } catch (error) {
-      next(error);
     }
   };
 
