c8ec39cbc9314c96021cdedf70870e898843459a
Yeasin Arafat <leoarafat10@gmail.com>
2024-05-28 15:19:09 +0600

    refactor auth middleware

--- src/app/modules/messages/message.model.ts
+++ src/app/modules/messages/message.model.ts
@@ -7,11 +7,11 @@ const messageSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
-    receiverId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
+    // receiverId: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: 'User',
+    //   required: true,
+    // },
     conversationId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Conversation',
--- src/app/modules/messages/message.service.ts
+++ src/app/modules/messages/message.service.ts
@@ -5,66 +5,118 @@ import Message from './message.model';
 import User from '../user/user.model';
 import ApiError from '../../../errors/ApiError';
 import { io } from '../../../socket/socket';
+import httpStatus from 'http-status';
 
 //! One to one conversation
+// const sendMessage = async (req: Request) => {
+//   try {
+//     // const { message } = req.body;
+//     const { id: receiverId } = req.params;
+//     const senderId = req.user?.userId;
+//     const { files } = req;
+//     const data = req.body;
+
+//     const { message } = data;
+
+//     const checkReceiverUser = await User.findById(receiverId);
+//     const checkSenderUser = await User.findById(senderId);
+
+//     if (checkReceiverUser === null || checkSenderUser === null) {
+//       throw new ApiError(404, 'Sender or Receiver user not found');
+//     }
+
+//     let conversation = await Conversation.findOne({
+//       participants: { $all: [senderId, receiverId] },
+//       isGroup: false,
+//     });
+//     // console.log(conversation, 'conversation');
+//     if (!conversation) {
+//       conversation = await Conversation.create({
+//         participants: [senderId, receiverId],
+//       });
+//     }
+//     let image = undefined;
+
+//     //@ts-ignore
+//     if (files && files?.image) {
+//       //@ts-ignore
+
+//       image = `/images/image/${files.image[0].filename}`;
+//     }
+//     const newMessage = new Message({
+//       senderId,
+//       receiverId,
+//       message,
+//       conversationId: conversation._id,
+//       image,
+//     });
+
+//     if (newMessage) {
+//       conversation.messages.push(newMessage._id);
+//     }
+//     await Promise.all([conversation.save(), newMessage.save()]);
+
+//     if (conversation && newMessage) {
+//       //@ts-ignore
+//       io.to(receiverId).emit('getMessage', newMessage);
+//     }
+
+//     return newMessage;
+//   } catch (error) {
+//     //@ts-ignore
+//     console.log('Error in sendMessage controller: ', error?.message);
+//   }
+// };
+//!
 const sendMessage = async (req: Request) => {
-  try {
-    // const { message } = req.body;
-    const { id: receiverId } = req.params;
-    const senderId = req.user?.userId;
-    const { files } = req;
-    const data = req.body;
+  const senderId = req.user?.userId;
+  const { files } = req;
+  const data = req.body;
 
-    const { message } = data;
+  const { message } = data;
 
-    const checkReceiverUser = await User.findById(receiverId);
-    const checkSenderUser = await User.findById(senderId);
+  const checkSenderUser = await User.findById(senderId);
 
-    if (checkReceiverUser === null || checkSenderUser === null) {
-      throw new ApiError(404, 'Sender or Receiver user not found');
-    }
-
-    let conversation = await Conversation.findOne({
-      participants: { $all: [senderId, receiverId] },
-      isGroup: false,
-    });
-    // console.log(conversation, 'conversation');
-    if (!conversation) {
-      conversation = await Conversation.create({
-        participants: [senderId, receiverId],
-      });
-    }
-    let image = undefined;
-
-    //@ts-ignore
-    if (files && files?.image) {
-      //@ts-ignore
-
-      image = `/images/image/${files.image[0].filename}`;
-    }
-    const newMessage = new Message({
-      senderId,
-      receiverId,
-      message,
-      conversationId: conversation._id,
-      image,
-    });
-
-    if (newMessage) {
-      conversation.messages.push(newMessage._id);
-    }
-    await Promise.all([conversation.save(), newMessage.save()]);
-
-    if (conversation && newMessage) {
-      //@ts-ignore
-      io.to(receiverId).emit('getMessage', newMessage);
-    }
-
-    return newMessage;
-  } catch (error) {
-    //@ts-ignore
-    console.log('Error in sendMessage controller: ', error?.message);
+  if (!checkSenderUser) {
+    throw new ApiError(404, 'Sender not found');
   }
+
+  const conversation = await Conversation.findOne({
+    participants: { $all: [senderId] },
+    isGroup: false,
+  });
+
+  if (!conversation) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Conversation not found');
+  }
+  let image = undefined;
+
+  //@ts-ignore
+  if (files && files?.image) {
+    //@ts-ignore
+
+    image = `/images/image/${files.image[0].filename}`;
+  }
+  const newMessage = new Message({
+    senderId,
+    message,
+    conversationId: conversation._id,
+    image,
+  });
+
+  await (await newMessage.populate('senderId')).populate('conversationId');
+
+  if (newMessage) {
+    conversation.messages.push(newMessage._id);
+  }
+  await Promise.all([conversation.save(), newMessage.save()]);
+
+  if (conversation && newMessage) {
+    //@ts-ignore
+    io.to(conversation?._id).emit('getMessage', newMessage);
+  }
+
+  return newMessage;
 };
 //!
 const getMessages = async (req: Request, res: Response) => {
